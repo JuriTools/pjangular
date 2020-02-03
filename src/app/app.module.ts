d7272e892e2f07ec525365550e5156dccde86785
Mathijs van Westendorp <mvwestendorp@gmail.com>
2020-02-03 09:53:50 +0100

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,46 +1,52 @@
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {
-  MatButtonModule,
-  MatButtonToggleModule,
-  MatCardModule,
-  MatCheckboxModule,
-  MatExpansionModule,
-  MatFormFieldModule,
-  MatIconModule,
-  MatInputModule,
-  MatMenuModule,
-  MatSidenavModule,
-  MatToolbarModule,
-  MatTreeModule
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatTreeModule
 } from '@angular/material';
-import { NgModule } from '@angular/core';
-import { HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-import { EjLawComponent } from './ej-law/ej-law.component';
-import { SafeHtmlPipe } from './safe-html.pipe';
-import { EjArticleComponent } from './ej-article/ej-article.component';
-import { EjContainerComponent } from './ej-container/ej-container.component';
-import { TocContainerComponent } from './toc-container/toc-container.component';
+import {AppComponent} from './app.component';
+import {EjLawComponent} from './ej-law/ej-law.component';
+import {SafeHtmlPipe} from './safe-html.pipe';
+import {EjArticleComponent} from './ej-article/ej-article.component';
+import {EjContainerComponent} from './ej-container/ej-container.component';
+import {TocContainerComponent} from './toc-container/toc-container.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    EjLawComponent,
-    SafeHtmlPipe,
-    EjArticleComponent,
-    EjContainerComponent,
-    TocContainerComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    MatButtonModule, MatCheckboxModule, MatCardModule,
-    MatSidenavModule, MatMenuModule, MatIconModule, MatToolbarModule, MatTreeModule, MatFormFieldModule, MatInputModule, MatButtonToggleModule, MatExpansionModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        EjLawComponent,
+        SafeHtmlPipe,
+        EjArticleComponent,
+        EjContainerComponent,
+        TocContainerComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        MatButtonModule, MatCheckboxModule, MatCardModule,
+        MatSidenavModule, MatMenuModule, MatIconModule, MatToolbarModule, MatTreeModule, MatFormFieldModule, MatInputModule, MatButtonToggleModule, MatExpansionModule,
+        RouterModule.forRoot([
+            {path: '', component: AppComponent},
+            {path: 'law', component: EjLawComponent}
+        ]),
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
