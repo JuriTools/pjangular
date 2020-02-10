dd28d2beeae899f2be0b68150390a6dac063c96a
Mathijs van Westendorp <mvwestendorp@gmail.com>
2020-02-10 11:51:09 +0100

    Add range to layout in both ToC and Container titles Change ToC layout

--- src/app/toc-container/toc-container.component.ts
+++ src/app/toc-container/toc-container.component.ts
@@ -3,18 +3,51 @@ import {Article} from '../article';
 import {Container} from '../law';
 
 @Component({
-  selector: 'app-toc-container',
-  templateUrl: './toc-container.component.html',
-  styleUrls: ['./toc-container.component.scss']
+    selector: 'app-toc-container',
+    templateUrl: './toc-container.component.html',
+    styleUrls: ['./toc-container.component.scss']
 })
 export class TocContainerComponent implements OnInit {
-  @Input() items: Container[] | Article[];
-  constructor() { }
+    @Input() items: Container[] | Article[];
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
 
-    getIsArticle(item) {
-        return item.constructor.name === 'Article';
+    ngOnInit() {
+    }
+
+    isArticle(item) {
+        return item?.classId === 'Article';
+    }
+
+    isContainer(item) {
+        return item?.classId === 'Container';
+    }
+
+    getRange(): { first: Container | Article, last: Container | Article } {
+        return {
+            first: this.getFirstItem(),
+            last: this.getLastItem()
+        };
+    }
+
+    getFirstItem(): Container | Article {
+        const firstItem = this.items[0];
+        return firstItem;
+    }
+
+    getLastItem(): Container | Article {
+        const lastItem = this.items[this.items.length - 1];
+        return lastItem;
+    }
+
+    getFirstChild(): Container | Article {
+        const firstItem = this.items[0];
+        return firstItem;
+    }
+
+    getLastChild(): Container | Article {
+        const lastItem = this.items[this.items.length - 1];
+        return lastItem;
     }
 }
